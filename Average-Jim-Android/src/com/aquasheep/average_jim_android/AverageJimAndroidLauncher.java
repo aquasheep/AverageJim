0324341e482e45dd7121af9f815648d5e52b44c8
Aquasheep <kennepm0@sewanee.edu>
2012-08-19 16:10:49 -0500

    Project files set-up for Android and Desktop AverageJim

---
+++ Average-Jim-Android/src/com/aquasheep/average_jim_android/AverageJimAndroidLauncher.java
@@ -0,0 +1,20 @@
+package com.aquasheep.average_jim_android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AverageJimAndroidLauncher extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_average_jim_android_launcher);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_average_jim_android_launcher, menu);
+        return true;
+    }
+}
